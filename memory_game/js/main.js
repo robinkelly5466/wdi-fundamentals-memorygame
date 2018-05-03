e25a26abd6338ea535dd3d9d1e5613bfa1fcf61d
Kelly <RM26290@ad1.prod>
2018-05-03 18:29:36 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,41 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        card: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        card: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        card: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        card: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 var cardsInPlay = [];
 var checkForMatch = function () {
-    if (cardsInPlay[0] === cardsInPlay[2]) {
-    alert("You found a match!");
-} else {
-    alert("Sorry, try again.");
-}
-};  
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.");
+    }
+};
 var flipCard = function (cardId) {
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].card);
+    cardsInPlay.push(cards[cardId].card);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
     if (cardsInPlay.length === 2) {
         checkForMatch();
     }
 };
 flipCard(0);
-flipCard(1);
+flipCard(2);
