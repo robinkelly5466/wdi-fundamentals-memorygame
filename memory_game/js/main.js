eb507aaa099cc8aefcc9331bd618cc6ecbaf719a
Kelly <RM26290@ad1.prod>
2018-05-03 21:10:29 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -28,14 +28,23 @@ var checkForMatch = function () {
         alert("Sorry, try again.");
     }
 };
-var flipCard = function (cardId) {
-    console.log("User flipped " + cards[cardId].card);
+var flipCard = function () {
+    cardId = this.getAttribute("data-id");
+    console.log(cardId);
     cardsInPlay.push(cards[cardId].card);
-    console.log(cards[cardId].cardImage);
-    console.log(cards[cardId].suit);
+    this.setAttribute("src", cards[cardId].cardImage);
     if (cardsInPlay.length === 2) {
         checkForMatch();
+        cardsInPlay = [];
     }
 };
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement("img");
+        cardElement.setAttribute("src", "images/back.png");
+        cardElement.setAttribute("data-id", i);
+        cardElement.addEventListener("click", flipCard);
+        document.getElementById("game-board").appendChild(cardElement);
+    }
+}
+createBoard();
